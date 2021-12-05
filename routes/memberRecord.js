9e58dd6f0c1f23c20caf6a6e32443811bb75a997
danniellu <84487618+danniellu@users.noreply.github.com>
2021-12-05 19:09:18 +0800

    更新完成訂單更新紅利 (#25)
    * 新增完成訂單更新紅利

--- routes/memberRecord.js
+++ routes/memberRecord.js
@@ -10,7 +10,7 @@ router.use(loginCheckMiddleware)
 // 取得會員訂單進行中資料
 router.get('/carryOut', async (req, res) => {
   let data = await connection.queryAsync(
-    'SELECT order_detail.pet_sitter_id, order_detail.order_status, order_detail.district, order_detail.address, order_detail.start, order_detail.end, order_detail.title AS price, order_detail.pet_id AS petName FROM order_detail INNER JOIN order_list ON order_list.id = order_detail.order_id WHERE order_list.member_id = ? AND order_list.check_status = ? AND order_detail.order_status = ?',
+    'SELECT order_detail.id, order_detail.pet_sitter_id, order_detail.order_status, order_detail.district, order_detail.address, order_detail.start, order_detail.end, order_detail.title AS price, order_detail.pet_id AS petName FROM order_detail INNER JOIN order_list ON order_list.id = order_detail.order_id WHERE order_list.member_id = ? AND order_list.check_status = ? AND order_detail.order_status = ?',
     [req.session.member.id, 1, 1]
   )
 
@@ -33,7 +33,7 @@ router.get('/carryOut', async (req, res) => {
 // 取得會員訂單已完成資料
 router.get('/complete', async (req, res) => {
   let data = await connection.queryAsync(
-    'SELECT order_detail.pet_sitter_id, order_detail.order_status, order_detail.district, order_detail.address, order_detail.start, order_detail.end, order_detail.title AS price, order_detail.pet_id AS petName FROM order_detail INNER JOIN order_list ON order_list.id = order_detail.order_id WHERE order_list.member_id = ? AND order_list.check_status = ? AND order_detail.order_status = ?',
+    'SELECT order_detail.id, order_detail.pet_sitter_id, order_detail.order_status, order_detail.district, order_detail.address, order_detail.start, order_detail.end, order_detail.title AS price, order_detail.pet_id AS petName FROM order_detail INNER JOIN order_list ON order_list.id = order_detail.order_id WHERE order_list.member_id = ? AND order_list.check_status = ? AND order_detail.order_status = ?',
     [req.session.member.id, 1, 2]
   )
 
@@ -56,7 +56,7 @@ router.get('/complete', async (req, res) => {
 // 取得會員訂單已取消資料
 router.get('/cancel', async (req, res) => {
   let data = await connection.queryAsync(
-    'SELECT order_detail.pet_sitter_id, order_detail.order_status, order_detail.district, order_detail.address, order_detail.start, order_detail.end, order_detail.title AS price, order_detail.pet_id AS petName FROM order_detail INNER JOIN order_list ON order_list.id = order_detail.order_id WHERE order_list.member_id = ? AND order_list.check_status = ? AND order_detail.order_status = ?',
+    'SELECT order_detail.id, order_detail.pet_sitter_id, order_detail.order_status, order_detail.district, order_detail.address, order_detail.start, order_detail.end, order_detail.title AS price, order_detail.pet_id AS petName FROM order_detail INNER JOIN order_list ON order_list.id = order_detail.order_id WHERE order_list.member_id = ? AND order_list.check_status = ? AND order_detail.order_status = ?',
     [req.session.member.id, 1, 3]
   )
 
@@ -81,7 +81,7 @@ router.post('/evaluation', async (req, res) => {
   console.log('req,body', req.body)
 
   let data = await connection.queryAsync(
-    `INSERT INTO evaluation (member_id, pet_sitter_id, content, score, created_time) VALUES (?, ?, ?, ?, ?)`,
+    `INSERT INTO evaluation (member_id, pet_sitter_id, content, score, create_time) VALUES (?, ?, ?, ?, ?)`,
     [
       req.session.member.id,
       req.body.pet_sitter_id,
@@ -92,6 +92,43 @@ router.post('/evaluation', async (req, res) => {
   )
 
   res.json({ code: '6000', message: '評價新增成功' })
+})
+
+// 完成訂單
+router.post('/finish', async (req, res) => {
+  console.log('req,body', req.body)
+
+  // 將訂單改為已完成
+  let data = await connection.queryAsync(
+    `UPDATE order_detail SET order_status = ? WHERE id= ?`,
+    [2, req.body.id]
+  )
+
+  // 將紅利更新
+  let bonus = await connection.queryAsync(
+    'SELECT member.total_bonus, order_detail.title AS price FROM member INNER JOIN order_list ON member.id = order_list.member_id INNER JOIN order_detail ON order_list.id = order_detail.order_id WHERE order_detail.id = ? AND order_detail.order_status = ?',
+    [req.body.id, 2]
+  )
+
+  bonus = bonus[0]
+  let updateBonus = bonus.total_bonus + Math.floor(Number(bonus.price) / 100)
+
+  let updateMemberBonus = await connection.queryAsync(
+    `UPDATE member SET total_bonus = ? WHERE id= ?`,
+    [updateBonus, req.session.member.id]
+  )
+
+  res.json({ code: '6001', message: '訂單已完成 將通知賣家' })
+})
+
+// 取消訂單
+router.post('/cancelOrder', async (req, res) => {
+  console.log('req,body', req.body)
+  let data = await connection.queryAsync(
+    `UPDATE order_detail SET order_status = ? WHERE id= ?`,
+    [3, req.body.id]
+  )
+  res.json({ code: '6002', message: '訂單已取消 將通知賣家' })
 })
 
 module.exports = router
