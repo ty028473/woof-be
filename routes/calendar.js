7cf9243efca7c7204be76b371a03eb53babc9aa8
danniellu <84487618+danniellu@users.noreply.github.com>
2021-12-06 22:32:33 +0800

    Member (#30)

--- routes/calendar.js
+++ routes/calendar.js
@@ -4,17 +4,13 @@ const connection = require('./connection')
 const moment = require('moment')
 //顯示保母時段（後台）
 router.get('/sitter/:timeId', async (req, res) => {
-  let data = await connection.queryAsync(
-    'SELECT * FROM time WHERE pet_sitter_id=?',
-    [req.params.timeId]
-  )
-
-  if (data.length > 0) {
-    //回一個物件
+  try {
+    let data = await connection.queryAsync(
+      'SELECT * FROM time WHERE pet_sitter_id=?',
+      [req.params.timeId]
+    )
     res.json(data)
-  } else {
-    //empty=44
-    //ex:api/Reserve/33
+  } catch (e) {
     res.status(404).send('找不到頁面')
   }
 })
@@ -59,19 +55,29 @@ router.post('/sitter/time_removed', async (req, res) => {
 
 //顯示保母時段（前台）
 router.get('/:timeId', async (req, res) => {
-  let data = await connection.queryAsync(
-    'SELECT * FROM time WHERE pet_sitter_id=?',
-    [req.params.timeId]
-  )
-  console.log(req.params.timeId)
-  if (data.length > 0) {
-    //回一個物件
+  try {
+    let data = await connection.queryAsync(
+      'SELECT * FROM time WHERE pet_sitter_id=?',
+      [req.params.timeId]
+    )
+    console.log(req.params.timeId)
     res.json(data)
-  } else {
-    //empty=44
-    //ex:api/Reserve/33
+  } catch (e) {
     res.status(404).send('找不到頁面')
   }
+  // let data = await connection.queryAsync(
+  //   'SELECT * FROM time WHERE pet_sitter_id=?',
+  //   [req.params.timeId]
+  // )
+  // console.log(req.params.timeId)
+  // if (data.length > 0) {
+  //   //回一個物件
+  //   res.json(data)
+  // } else {
+  //   //empty=44
+  //   //ex:api/Reserve/33
+  //   res.status(404).send('找不到頁面')
+  // }
 })
 
 module.exports = router
