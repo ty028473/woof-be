d4f4766ceb1989a8855abd73723467b2cd9cbd34
danlu <dandesign0818@gmail.com>
2021-12-07 13:10:01 +0800

    從創建時間排序

--- routes/petSitterRecord.js
+++ routes/petSitterRecord.js
@@ -10,7 +10,7 @@ router.use(loginCheckMiddleware)
 // 取得保母訂單進行中資料
 router.get('/carryOut', async (req, res) => {
   let data = await connection.queryAsync(
-    'SELECT order_detail.id, order_list.member_id, order_detail.order_status, order_detail.district, order_detail.address, order_detail.start, order_detail.end, order_detail.title AS price, order_detail.pet_id AS petName, order_detail.evaluation_states FROM order_detail INNER JOIN order_list ON order_list.id = order_detail.order_id WHERE order_detail.pet_sitter_id = ? AND order_list.check_status = ? AND order_detail.order_status = ?',
+    'SELECT order_detail.id, order_list.member_id, order_detail.order_status, order_detail.district, order_detail.address, order_detail.start, order_detail.end, order_detail.title AS price, order_detail.pet_id AS petName, order_detail.evaluation_states FROM order_detail INNER JOIN order_list ON order_list.id = order_detail.order_id WHERE order_detail.pet_sitter_id = ? AND order_list.check_status = ? AND order_detail.order_status = ? ORDER BY order_list.create_time DESC',
     [req.session.member.petSitterId, 1, 1]
   )
 
@@ -33,7 +33,7 @@ router.get('/carryOut', async (req, res) => {
 // 取得會員訂單已完成資料
 router.get('/complete', async (req, res) => {
   let data = await connection.queryAsync(
-    'SELECT order_detail.id, order_list.member_id, order_detail.order_status, order_detail.district, order_detail.address, order_detail.start, order_detail.end, order_detail.title AS price, order_detail.pet_id AS petName, order_detail.evaluation_states FROM order_detail INNER JOIN order_list ON order_list.id = order_detail.order_id WHERE order_detail.pet_sitter_id = ? AND order_list.check_status = ? AND order_detail.order_status = ?',
+    'SELECT order_detail.id, order_list.member_id, order_detail.order_status, order_detail.district, order_detail.address, order_detail.start, order_detail.end, order_detail.title AS price, order_detail.pet_id AS petName, order_detail.evaluation_states FROM order_detail INNER JOIN order_list ON order_list.id = order_detail.order_id WHERE order_detail.pet_sitter_id = ? AND order_list.check_status = ? AND order_detail.order_status = ? ORDER BY order_list.create_time DESC',
     [req.session.member.id, 1, 2]
   )
 
@@ -56,7 +56,7 @@ router.get('/complete', async (req, res) => {
 // 取得會員訂單已取消資料
 router.get('/cancel', async (req, res) => {
   let data = await connection.queryAsync(
-    'SELECT order_detail.id, order_list.member_id, order_detail.order_status, order_detail.district, order_detail.address, order_detail.start, order_detail.end, order_detail.title AS price, order_detail.pet_id AS petName, order_detail.evaluation_states FROM order_detail INNER JOIN order_list ON order_list.id = order_detail.order_id WHERE order_detail.pet_sitter_id = ? AND order_list.check_status = ? AND order_detail.order_status = ?',
+    'SELECT order_detail.id, order_list.member_id, order_detail.order_status, order_detail.district, order_detail.address, order_detail.start, order_detail.end, order_detail.title AS price, order_detail.pet_id AS petName, order_detail.evaluation_states FROM order_detail INNER JOIN order_list ON order_list.id = order_detail.order_id WHERE order_detail.pet_sitter_id = ? AND order_list.check_status = ? AND order_detail.order_status = ? ORDER BY order_list.create_time DESC',
     [req.session.member.id, 1, 3]
   )
 
